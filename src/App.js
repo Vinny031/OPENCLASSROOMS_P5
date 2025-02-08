b78ed50bfc22b97cf38ae9eff284d2eb2a44545b
Vincent Fuseau <vincent.fuseau@hotmail.fr>
2025-02-08 12:55:12 +0100

    Modification des fichiers App.js et index.js (racine) pour inclure une redirection vers la page d'erreur 404 si url non fonctionnelle. Suppression du router supperflu dans App.js.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/index.jsx";
 import About from "./pages/About/index.jsx";
+import Error from "./pages/Error/index.jsx";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/error" element={<Error />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
   );
 };
 
---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,10 @@
+const Error = () => {
+    return (
+        <div>
+            <h1>ERROR 404</h1>
+            <p>Page not found</p>
+        </div>
+    )
+};
+  
+  export default Error;
