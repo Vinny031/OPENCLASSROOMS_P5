7a28734dc827184e07b7686a20767a1727a4e93a
Vincent Fuseau <vincent.fuseau@hotmail.fr>
2025-02-08 14:31:45 +0100

    Ajout de la partie banner (image et titre) dans la home page.

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,12 @@
+import banner from '../../assets/HOME_PAGE.png';
+
+const Banner = () => {
+    return (
+    <div>
+        <img src={banner} alt="Photo d'une côte sauvage avec l'océan." />
+        <h1>Chez vous, partout et ailleurs</h1>
+    </div>
+    )
+};
+
+export default Banner;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,11 @@
 import Header from "../../components/Header";
+import Banner from "../../components/Banner";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <div>
-        <h1>Bienvenue sur la page d'accueil</h1>
-      </div>
+      <Banner />
     </>
   )
 };
