3236b1bee33cfd9eb2d9487a5b3300dd1718e435
Vincent Fuseau <vincent.fuseau@hotmail.fr>
2025-02-10 10:52:37 +0100

    Ajout du footer sur la homepage et du header+footer sur les pages about et error.

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,10 +1,20 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 import { useEffect } from "react";
 
 const About = () => {
     useEffect(() => {
       document.title = "À propos | Kasa";
     }, []);
-    return <h1>À propos</h1>;
+    return (
+      <>
+          <Header />
+          <div>
+            <h1>À propos</h1>;
+          </div>
+          <Footer />
+      </>
+    )
   };
-  
+
   export default About;
