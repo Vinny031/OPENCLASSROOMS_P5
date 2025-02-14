3c7de84bb70d6ef0699c69d7c896c005953157bb
Vincent Fuseau <vincent.fuseau@hotmail.fr>
2025-02-14 09:38:49 +0100

    Ajout du style pour le carroussel. Lien vers le main.scss effectué.

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -4,23 +4,29 @@ import ABOUT_PAGE from '../../assets/ABOUT_PAGE.png';
 import Banner from '../../components/Banner';
 import Collapse from '../../components/Collapse';
 import About_Data from '../../data/about-collapse.json';
+import { useEffect } from "react";
 
 
-const About = () => (
-  <>
-  <Header />
-  <div className="about">
-    <Banner image={ABOUT_PAGE} alt="Un paysage de montagnes avec une rivière serpentant à travers, bordée d'arbres verdoyants et de cimes enneigées en arrière-plan."/>
-    <div>
-      {About_Data.sections.map((item) => (
-          <Collapse key={item.index} title={item.title}>
-            <p>{item.content}</p>
-          </Collapse>
-      ))}
+const About = () => {
+  useEffect(() => {
+      document.title = "À propos | Kasa";
+    }, []);
+  return (
+    <>
+    <Header />
+    <div className="about">
+      <Banner image={ABOUT_PAGE} alt="Un paysage de montagnes avec une rivière serpentant à travers, bordée d'arbres verdoyants et de cimes enneigées en arrière-plan."/>
+      <div>
+        {About_Data.sections.map((item) => (
+            <Collapse key={item.index} title={item.title}>
+              <p>{item.content}</p>
+            </Collapse>
+        ))}
+      </div>
     </div>
-  </div>
-  <Footer />
-  </>
-);
+    <Footer />
+    </>
+)
+};
 
 export default About;
