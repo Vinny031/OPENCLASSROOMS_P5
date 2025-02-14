4490df2fc0094a9fdee34ece509591ca22ea4286
Vincent Fuseau <vincent.fuseau@hotmail.fr>
2025-02-14 09:05:40 +0100

    Ajout de la possibilité de naviguer au clavier entre les différents éléments du carroussel.

--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Slideshow({ images }) {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -14,7 +14,24 @@ function Slideshow({ images }) {
       prevIndex === images.length - 1 ? 0 : prevIndex + 1
     );
   };
-  
+
+  // --- Gestion des touches clavier pour la navigation dans le carroussel. Vérification de la touche pressée par event.key ---
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "ArrowLeft") {
+        prevSlide();
+      } else if (event.key === "ArrowRight") {
+        nextSlide();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  // --- Ajout dépendance sur currentIndex pour garder le state à jour ---
+  }, [currentIndex]);
 
   return (
     <div className="slider">
