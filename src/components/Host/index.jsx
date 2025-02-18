147cd3ff4b5ed777bb22e9f0db61d9e605e98e99
Vincent Fuseau <vincent.fuseau@hotmail.fr>
2025-02-18 17:17:50 +0100

    Suppression du dossier nom_du_dossier

--- src/components/Host/index.jsx
+++
@@ -1,9 +0,0 @@
-const Host= ({ host }) => (
-  <div className="host-info">
-    <p className="host-name">{host.name}</p>
-    <img src={host.picture} alt={host.name} className="host-image" />
-    
-  </div>
-);
-
-export default Host;
